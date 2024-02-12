cacb80079b18ffa529149c3412b296885bf5a907
Manrico-Tizzani <116350929+Nicholas1K@users.noreply.github.com>
2024-02-12 09:30:54 +0100

    Create action.jsx
    Nel file del action ci sono i comandi per aggiungere o rimuovere un servizio che poi verranno utilizzati dal actionCreator e dal reducer

---
+++ redux/action.jsx
@@ -0,0 +1,6 @@
+export const SELEZIONE_SERVIZIO = {
+  DATA_SUCCESS:'get_aggiunta_servizio_success',
+};
+export const RIMUOVI_SELEZIONE_SERVIZIO = {
+  DATA_SUCCESS:'get_rimuove_servizio_success',
+};
